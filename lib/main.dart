6515cc70120198f198280c78c61edb48627f2c97
neilhogg <6nDpEsWPqJegskZF7sG_>
2022-11-02 12:58:46 +0200

    Update to the onTapDown method and removed comments

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image tracker demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Offset? tapPosition;
   Size imageWidgetSize = Size.zero;
 
   @override
@@ -51,14 +39,14 @@ class _MyHomePageState extends State<MyHomePage> {
     final imageWidget = Image.asset('assets/test.png');
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Image tracker demo'),
       ),
       body: Center(
         child: GestureDetector(
           onTap: () {},
           onTapDown: (details) async {
             if (imageWidgetSize != Size.zero) {
-              tapPosition = details.localPosition;
+              final tapPosition = details.localPosition;
               final assetSize = await _getImageSize(imageWidget);
               final transformSize = _getTransformSize(assetSize, imageWidgetSize);
 
@@ -66,8 +54,8 @@ class _MyHomePageState extends State<MyHomePage> {
               print('widgetTapPosition [$tapPosition]');
               print('imageSize [$assetSize]');
 
-              final transformedX = (tapPosition?.dx ?? 1) * transformSize.width;
-              final transformedY = (tapPosition?.dy ?? 1) * transformSize.height;
+              final transformedX = tapPosition.dx * transformSize.width;
+              final transformedY = tapPosition.dy * transformSize.height;
               print('transformedPosition[$transformedX, $transformedY]');
 
               setState(() {});
